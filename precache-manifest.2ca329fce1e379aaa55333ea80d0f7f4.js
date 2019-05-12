43aed8f0c2bb06ae10ede6db2bed382db29dcd91
rlisboars <rlisboars@gmail.com>
2019-05-12 18:00:55 -0300

    Updates

---
+++ precache-manifest.2ca329fce1e379aaa55333ea80d0f7f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0908fa516156fa0bc8aa0bc50d8d3c51",
+    "url": "/react-githubmap/index.html"
+  },
+  {
+    "revision": "e4b1ff592a637e68c9ab",
+    "url": "/react-githubmap/static/css/2.f4948e68.chunk.css"
+  },
+  {
+    "revision": "e4b1ff592a637e68c9ab",
+    "url": "/react-githubmap/static/js/2.691be866.chunk.js"
+  },
+  {
+    "revision": "54ab8de194f5b12327c7",
+    "url": "/react-githubmap/static/js/main.e371fbbf.chunk.js"
+  },
+  {
+    "revision": "57b6180e2b9b9513a18e",
+    "url": "/react-githubmap/static/js/runtime~main.0b32cadd.js"
+  }
+]);
